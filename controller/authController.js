1f89c9025eb73e1bb51f9d18d8038ca483406f41
Azat Seyrek <azatseyrek@hotmail.com>
2022-01-09 22:43:32 +0300

    login and signup page

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,30 @@
 const User = require("../models/User");
 
+// handle errors
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: "", password: "" };
+
+  //duplicate err code
+  
+  //error message: E11000 duplicate key error collection: node-auth.users index: email_1 dup key: { email: "miranali@asd.com" } 11000
+  if(err.code === 11000) {
+    errors.email = 'that email is already registered'
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes("user validation failed")) {
+    // console.log(err.errors.email.properties.message)
+
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
+
 module.exports.signupGet = (req, res) => {
   res.render("signup");
 };
@@ -11,13 +36,12 @@ module.exports.signupPost = async (req, res) => {
       email: email,
       password: password,
     });
-    res.status(201)
-    res.send(user)
-
-
+    res.status(201);
+    res.send(user);
   } catch (err) {
-res.status(400)
-res.send("error, user not created")
+    const errors = handleErrors(err); // this is a cb function for catching the err.message which is came from to our User model
+    res.status(400);
+    res.send({errors});
   }
 };
 module.exports.loginGet = (req, res) => {
@@ -25,12 +49,12 @@ module.exports.loginGet = (req, res) => {
 };
 module.exports.loginPost = async (req, res) => {
   const { email, password } = req.body;
- 
+
   try {
     const user = User.create({
       email: email,
-      password: password
-    })
+      password: password,
+    });
   } catch (err) {
     console.log(error);
   }
--- models/User.js
+++ models/User.js
@@ -1,19 +1,41 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt"); //for hash the passwords
 
 // create Schema
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"], //second parameter is err.message which we used in authController cb functions
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Please enter a valid email"], //second parameter is err.message which we used in authController cb functions
   },
   password: {
     type: String,
-    require: true,
-    minLength: 6,
+    require: [true, "Please enter a password"],
+    minlength: [6, "Minimum password length is 6 characters"],
   },
+});
+
+// Fire a function after a doc saved to db
+//This is a mongoose hook. Mongoose  hooks can be used to fire code at different points when documents are saved to the db
+userSchema.post("save", function (doc, next) {
+  console.log("New user was created & saved", doc);
+  next();
+  //the next parameter is a function provided to you by mongoose to have a way out, or to tell mongoose you are done and to continue with the next step in the execution chain.
+
+  // post middleware documantation: https://mongoosejs.com/docs/middleware.html#post
+});
+
+// fire a function before doc saved to db
+// we can hash password before to save data to db in this 'pre' hook
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt)
+  next();
+  
 });
 
 // Create Model
